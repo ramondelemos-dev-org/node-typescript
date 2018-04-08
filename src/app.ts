47bf85874fd0500565128e24d65a47e3700169d5
ramondelemos <ramondelemos@gmail.com>
2018-04-08 18:07:43 -0300

    Hello World.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import Hello from './routes/hello';
+
+// Criando as configurações para o ExpressJS
+class App {
+
+    // Instancia dele
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    // Configuração para o nosso middler
+    private middleware(): void {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    //Configuração da nossa API e nossos EndPoint e o famoso Hello 
+    private routes(): void {
+        this.express.use('/api/v1/hello', Hello);
+    }
+
+}
+
+export default new App().express;
